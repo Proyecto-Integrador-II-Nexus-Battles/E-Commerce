5cb59092dc94becccb15d7d6d2eff7d996f81327
Andres <Olaya4423@gmail.com>
2024-02-28 20:45:58 -0500

    siii

--- src/controllers/testing.controllers.js
+++ src/controllers/testing.controllers.js
@@ -2,12 +2,20 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export const defaultR = (req, res) => {
-  const datos = {
-    title: "Home",
-    message: "Hello World",
-  };
-  res.render("index");
+
+  const datos = [{
+    nombre: "Home",
+    poder: "Hello World",
+    vida: "si",
+    defensa: "si",
+    ataque: "si",
+    dano: "si",
+    imagen: "/img/cedric.jpg"
+  }];
+  console.log(datos)
+  res.render("index", datos);
 };
+
 
 export const defaultR2 = (req, res) => {
   res.render("admin_main_page");
