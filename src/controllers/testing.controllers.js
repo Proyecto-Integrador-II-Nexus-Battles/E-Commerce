512c546b7a9d124dd5d313971dde0a5341e56678
David Cárdenas <santito500@live.com>
2024-02-29 14:12:19 -0500

    Commit link href

--- src/controllers/testing.controllers.js
+++ src/controllers/testing.controllers.js
@@ -2,69 +2,68 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export const defaultR = (req, res) => {
-  const datos = [{
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  },
-  {
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  },
-  {
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  },
-  {
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  },
-  {
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  },
-  {
-    nombre: "Home",
-    poder: "Hello World",
-    vida: "si",
-    defensa: "si",
-    ataque: "si",
-    dano: "si",
-    imagen: "/img/cedric.jpg"
-  }
-
-];
-  console.log(datos)
-  res.render("index", {datos});
+  const datos = [
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+    {
+      nombre: "Home",
+      poder: "Hello World",
+      vida: "si",
+      defensa: "si",
+      ataque: "si",
+      dano: "si",
+      imagen: "/img/cedric.jpg",
+    },
+  ];
+  console.log(datos);
+  res.render("index", { datos });
 };
 
-
 export const defaultR2 = (req, res) => {
-  res.render("admin_main_page");
+  res.render("vistadetallada");
 };
 
 export const defaultR3 = (req, res) => {
