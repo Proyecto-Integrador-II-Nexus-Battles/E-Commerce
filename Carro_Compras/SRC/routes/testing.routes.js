97685f755520fe9052f652ee653c31accf9aaeec
David Cárdenas <santito500@live.com>
2024-03-01 10:36:25 -0500

    Cambios Estructura
    Cambios para ajustarse a la estructura MVC.

---
+++ Carro_Compras/SRC/routes/testing.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  defaultR,
+  defaultR2,
+  defaultR3,
+  defaultR4,
+} from "../controllers/testing.controllers.js";
+
+const router = Router();
+router.get("/", defaultR);
+router.get("/hola", defaultR2);
+router.get("/user", defaultR3);
+router.get("/loro", defaultR4);
+
+export default router;
