7187738a8ef0a5794e36caed02f8e45f8bd708c8
Shaj <hernandezarmentajuansebastian@gmail.com>
2024-03-01 00:09:46 -0500

    Mongo_Db

---
+++ src/controllers/cards.js
@@ -0,0 +1,10 @@
+// import { CardModel } from '../models/local-file-system/card.js' // -> LOCAL
+import { CardModel } from '../models/mongodb/card.js' // -> MONGODB
+
+export class CardsController {
+  static async getAll (req, res) {
+    const { TypeCard, minPrice, maxPrice, offer, sortOrder } = req.query
+    const cards = await CardModel.getAll({ TypeCard, minPrice, maxPrice, offer, sortOrder })
+    res.json(cards)
+  }
+}
